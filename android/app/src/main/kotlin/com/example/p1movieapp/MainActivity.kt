3862963d67813c5f361a43d39426bb02b4a1afa1
niranjandahal <niranjandahal76@gmail.com>
2025-01-14 21:06:44 +0545

    replcaing android and other directory

--- android/app/src/main/kotlin/com/example/p1movieapp/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.p1movieapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/example/r08fullmovieapp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.r08fullmovieapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
