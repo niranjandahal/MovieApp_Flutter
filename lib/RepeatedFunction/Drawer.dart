9a89573a0851b4a018f7dcd259462eae292aa9b3
niranjandahal <niranjandahal76@gmail.com>
2025-01-14 21:49:06 +0545

    work with latest version of flutter

--- lib/RepeatedFunction/Drawer.dart
+++ lib/RepeatedFunction/Drawer.dart
@@ -4,10 +4,12 @@ import 'package:r08fullmovieapp/SectionHomeUi/FavoriateList.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'repttext.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:io';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class drawerfunc extends StatefulWidget {
   const drawerfunc({
@@ -22,25 +24,19 @@ class _drawerfuncState extends State<drawerfunc> {
   File? _image;
 
   Future<void> SelectImage() async {
-    // final pickedfile =
-    //     await ImagePicker().(source: ImageSource.gallery);
-    // if (pickedfile != null) {
-    //   CroppedFile? cropped = await ImageCropper().cropImage(
-    //     sourcePath: pickedfile.path,
-    //     aspectRatioPresets: [
-    //       CropAspectRatioPreset.square,
-    //       CropAspectRatioPreset.ratio3x2,
-    //       CropAspectRatioPreset.original,
-    //       CropAspectRatioPreset.ratio4x3,
-    //       CropAspectRatioPreset.ratio16x9
-    //     ],
-    //   );
-    //   SharedPreferences sp = await SharedPreferences.getInstance();
-    //   sp.setString('imagepath', cropped!.path);
-    //   _image = cropped as File?;
-    // } else {
-    //   print('No image selected.');
-    // }
+    final pickedfile =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (pickedfile != null) {
+      CroppedFile? cropped = await ImageCropper().cropImage(
+        sourcePath: pickedfile.path,
+        aspectRatio: CropAspectRatio(ratioX: 1, ratioY: 1),
+      );
+      SharedPreferences sp = await SharedPreferences.getInstance();
+      sp.setString('imagepath', cropped!.path);
+      _image = cropped as File?;
+    } else {
+      print('No image selected.');
+    }
   }
 
   @override
@@ -53,6 +49,48 @@ class _drawerfuncState extends State<drawerfunc> {
     });
   }
 
+  final controllerone = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..setNavigationDelegate(
+      NavigationDelegate(
+        onProgress: (int progress) {
+          // Update loading bar.
+        },
+        onPageStarted: (String url) {},
+        onPageFinished: (String url) {},
+        onHttpError: (HttpResponseError error) {},
+        onWebResourceError: (WebResourceError error) {},
+        // onNavigationRequest: (NavigationRequest request) {
+        //   if (request.url.startsWith('https://www.youtube.com/')) {
+        //     return NavigationDecision.prevent;
+        //   }
+        //   return NavigationDecision.navigate;
+        // },
+      ),
+    )
+    ..loadRequest(Uri.parse('https://niranjandahal.com.np'));
+
+  final controllertwo = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..setNavigationDelegate(
+      NavigationDelegate(
+        onProgress: (int progress) {
+          // Update loading bar.
+        },
+        onPageStarted: (String url) {},
+        onPageFinished: (String url) {},
+        onHttpError: (HttpResponseError error) {},
+        onWebResourceError: (WebResourceError error) {},
+        // onNavigationRequest: (NavigationRequest request) {
+        //   if (request.url.startsWith('https://www.youtube.com/')) {
+        //     return NavigationDecision.prevent;
+        //   }
+        //   return NavigationDecision.navigate;
+        // },
+      ),
+    )
+    ..loadRequest(Uri.parse('https://dahalniranjan.com.np'));
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -62,6 +100,7 @@ class _drawerfuncState extends State<drawerfunc> {
           children: [
             DrawerHeader(
               child: Container(
+                height: 100,
                 child: Column(
                   children: [
                     GestureDetector(
@@ -79,15 +118,15 @@ class _drawerfuncState extends State<drawerfunc> {
                       },
                       child: _image == null
                           ? CircleAvatar(
-                              radius: 50,
+                              radius: 45,
                               backgroundImage: AssetImage('assets/user.png'),
                             )
                           : CircleAvatar(
-                              radius: 50,
+                              radius: 45,
                               backgroundImage: FileImage(_image!),
                             ),
                     ),
-                    SizedBox(height: 10),
+                    SizedBox(height: 6),
                     Text(
                       'Welcome',
                       style: TextStyle(color: Colors.white, fontSize: 20),
@@ -111,19 +150,12 @@ class _drawerfuncState extends State<drawerfunc> {
                   context,
                   MaterialPageRoute(
                       builder: (context) => Scaffold(
-                          backgroundColor: Color.fromRGBO(18, 18, 18, 0.5),
-                          appBar: AppBar(
-                            backgroundColor: Color.fromRGBO(18, 18, 18, 0.9),
-                            title: Text('Our Blogs'),
-                          ),
-                          body: WebViewWidget(
-                            controller: WebViewController(),
-                          )
-
-                          // body: WebView(
-                          //   initialUrl: 'https://niranjandahal.com.np/',
-                          //   javascriptMode: JavascriptMode.unrestricted,
-                          // ),
+                            backgroundColor: Color.fromRGBO(18, 18, 18, 0.5),
+                            appBar: AppBar(
+                              backgroundColor: Color.fromRGBO(18, 18, 18, 0.9),
+                              title: Text('Our Blogs'),
+                            ),
+                            body: WebViewWidget(controller: controllerone),
                           )));
             }),
             listtilefunc('Our Website', FontAwesomeIcons.solidNewspaper,
@@ -135,13 +167,9 @@ class _drawerfuncState extends State<drawerfunc> {
                             backgroundColor: Color.fromRGBO(18, 18, 18, 0.5),
                             appBar: AppBar(
                               backgroundColor: Color.fromRGBO(18, 18, 18, 0.9),
-                              title: Text('Our Website'),
+                              title: Text('flutter content'),
                             ),
-                            // body: WebView(
-                            //   initialUrl:
-                            //       'https://niranjandahalyt.blogspot.com/',
-                            //   javascriptMode: JavascriptMode.unrestricted,
-                            // ),
+                            body: WebViewWidget(controller: controllerone),
                           )));
             }),
             listtilefunc('Subscribe US', FontAwesomeIcons.youtube,
